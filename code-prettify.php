d48d82ca56cad013d74c173cfc6a76489a31891c
Kaspars <kaspars@konstruktors.com>
2013-05-23 14:17:17 +0200

    Add support for skins

--- code-prettify.php
+++ code-prettify.php
@@ -2,9 +2,9 @@
 /*
 	Plugin Name: Code Prettify
 	Plugin URI: https://github.com/kasparsd/code-prettify
-    GitHub URI: https://github.com/kasparsd/code-prettify
+	GitHub URI: https://github.com/kasparsd/code-prettify
 	Description: Automatic Syntax Highlighter
-	Version: 1.0
+	Version: 1.1
 	Author: Kaspars Dambis
 	Author URI: http://konstruktors.com
 */
@@ -12,24 +12,25 @@
 add_action( 'wp_enqueue_scripts', 'add_prettify_scripts' );
 
 function add_prettify_scripts() {
-    if ( WP_DEBUG )
-        $script = 'prettify-src.js';
-    else
-        $script = 'prettify.js';
+	if ( WP_DEBUG )
+		$script = 'prettify-src.js';
+	else
+		$script = 'prettify.js';
 
-    wp_enqueue_script( 
-    	'code-prettify', 
-    	sprintf( '%s/%s/%s', WP_PLUGIN_URL, basename( __DIR__ ), $script ), 
-    	false, 
-    	false, 
-    	true 
-    );
+	wp_enqueue_script( 
+		'code-prettify', 
+		sprintf( '%s/%s/%s', WP_PLUGIN_URL, basename( __DIR__ ), $script ), 
+		false, 
+		false, 
+		true 
+		);
 
-    wp_localize_script( 
-        'code-prettify', 
-        'code_prettify_settings', 
-        array( 
-            'base_url' => sprintf( '%s/%s', WP_PLUGIN_URL, basename( __DIR__ ) ) 
-        ) 
-   );
+	wp_localize_script( 
+		'code-prettify', 
+		'code_prettify_settings', 
+		array( 
+			'base_url' => sprintf( '%s/%s', WP_PLUGIN_URL, basename( __DIR__ ) ),
+			'skin' => apply_filters( 'prettify_skin', 'sunburst' )
+		)
+	);
 }
