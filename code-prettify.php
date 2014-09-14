6f409928e2c327da88895eb257f7eac7c83c9df0
root <root@li440-208.members.linode.com>
2014-09-14 01:59:01 +0000

    Upgrade to support https

--- code-prettify.php
+++ code-prettify.php
@@ -17,20 +17,21 @@ function add_prettify_scripts() {
 	else
 		$script = 'prettify.js';
 
-	wp_enqueue_script( 
-		'code-prettify', 
-		sprintf( '%s/%s/%s', WP_PLUGIN_URL, basename( __DIR__ ), $script ), 
-		false, 
-		false, 
-		true 
-		);
+	wp_enqueue_script(
+		'code-prettify',
+		sprintf( '%s/%s/%s', plugins_url(), basename( __DIR__ ), $script ),
+		false,
+		'1.0',
+		true
+	);
 
-	wp_localize_script( 
-		'code-prettify', 
-		'code_prettify_settings', 
-		array( 
-			'base_url' => sprintf( '%s/%s', WP_PLUGIN_URL, basename( __DIR__ ) ),
+	wp_localize_script(
+		'code-prettify',
+		'code_prettify_settings',
+		array(
+			'base_url' => sprintf( '%s/%s', plugins_url(), basename( __DIR__ ) ),
 			'skin' => apply_filters( 'prettify_skin', false )
 		)
 	);
 }
+
