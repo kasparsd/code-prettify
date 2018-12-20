b75acd488d29a1723f1c9edfdb8a359fbd9e441d
Kaspars Dambis <hi@kaspars.net>
2018-12-20 12:40:43 +0200

    Add defaults and skin support

--- code-prettify.php
+++ code-prettify.php
@@ -12,28 +12,22 @@
 add_action( 'wp_enqueue_scripts', 'add_prettify_scripts' );
 
 function add_prettify_scripts() {
-	$ver = '1.3.4';
-	$script = 'run_prettify.js';
+	$script_url = plugins_url( 'prettify/run_prettify.js', __FILE__ );
 
-	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-		$script = 'run_prettify-src.js';
+	$prettify_query_params = array_filter( array(
+		'skin' => apply_filters( 'prettify_skin', null ),
+		'autoload' => true,
+	) );
+
+	if ( ! empty( $prettify_query_params ) ) {
+		$script_url = add_query_arg( $prettify_query_params, $script_url );
 	}
-
-	$script_url = plugins_url( sprintf( 'prettify/%s', $script ), __FILE__ );
-
-	$skin = apply_filters( 'prettify_skin', null );
-
-	if ( $skin ) {
-		$script_url = add_query_arg( 'skin', $skin, $script_url );
-	}
-
-	$script_url = apply_filters( 'code-prettify-js-url', $script_url );
 
 	wp_enqueue_script(
 		'code-prettify',
-		$script_url,
+		apply_filters( 'code-prettify-js-url', $script_url ),
 		false,
-		$ver,
+		'1.4.0',
 		true
 	);
 
