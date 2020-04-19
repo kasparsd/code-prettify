476aee0493542535a7519eafe32acfdeb80c5b2c
Kaspars Dambis <hi@kaspars.net>
2020-04-19 11:02:57 +0300

    Apply WPCS

--- code-prettify.php
+++ code-prettify.php
@@ -14,9 +14,11 @@ add_action( 'wp_enqueue_scripts', 'add_prettify_scripts' );
 function add_prettify_scripts() {
 	$script_url = plugins_url( 'prettify/run_prettify.js', __FILE__ );
 
-	$prettify_query_params = array_filter( array(
-		'skin' => apply_filters( 'prettify_skin', null )
-	) );
+	$prettify_query_params = array_filter(
+		array(
+			'skin' => apply_filters( 'prettify_skin', null ),
+		)
+	);
 
 	if ( ! empty( $prettify_query_params ) ) {
 		$script_url = add_query_arg( $prettify_query_params, $script_url );
